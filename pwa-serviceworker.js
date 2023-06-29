335936d45236ad575620db898837dfb3bae57f83
kairi003 <kairi.satellite@gmail.com>
2023-06-30 01:43:23 +0900

    update

--- pwa-serviceworker.js
+++ pwa-serviceworker.js
@@ -1,25 +1,50 @@
-var CACHE_NAME = 'pwa-sample-caches';
-var urlsToCache = [
-  '/pwa/',
-  '/pwa/app.js',
+// Service Worker のバージョンとキャッシュする App Shell を定義する
+
+const NAME = 'pwa-example-cache';
+const VERSION = '1.0.0';
+const CACHE_NAME = NAME + ':' + VERSION;
+const urlsToCache = [
+  './',
+  './index.html',
+  './app.js',
 ];
+
+// Service Worker へファイルをインストール
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches
-      .open(CACHE_NAME)
+    caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
+// リクエストされたファイルが Service Worker にキャッシュされている場合
+// キャッシュからレスポンスを返す
+
+self.addEventListener('fetch', event =>  {
   if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin')
     return;
-  console.log(event.request);
-
   event.respondWith(
-    caches
-      .match(event.request)
+    caches.match(event.request)
       .then(response => response || fetch(event.request))
+  );
+});
+
+// キャッシュのバージョンが変わった場合、古いキャッシュを削除する
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => Promise.all(
+      cacheNames.map(cacheName => {
+        const parts = cacheName.split(':');
+        const version = parts.pop();
+        const name = parts.join(':');
+        if (name === NAME && version !== VERSION) {
+          return caches.delete(cacheName);
+        }
+      })
+    )).then(() => {
+      console.log(CACHE_NAME + "activated");
+    })
   );
 });
